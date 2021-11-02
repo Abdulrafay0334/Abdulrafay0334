9b00f3e2a491d25feca291e341bb63e09874d513
Abdulrafay0334 <92913641+Abdulrafay0334@users.noreply.github.com>
2021-11-02 11:23:59 +0500

    Create Gif of 2 images.m

---
+++ Gif of 2 images.m
@@ -0,0 +1,16 @@
+A = imread('abdul.jpg');
+B = imread('raffay.jpg');
+A = im2double(A);
+B = im2double(B);
+new = B - A;
+iter = 40;
+new = new ./ iter
+for i = 1 : iter
+    A = A + new;
+    [im,map] = rgb2ind(A,256);
+    if i == 1
+        imwrite(im,map,'Animated.gif','DelayTime',0.01,'LoopCount',1);
+    else
+        imwrite(im,map,'animated.gif','DelayTime',0.01,'WriteMode','Append');
+    end
+end
